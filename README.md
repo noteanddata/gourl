0c07c25ada324fe9cdd08b96449e73757ab4265c
noteanddata <noteanddata@users.noreply.github.com>
2019-08-02 20:08:27 -0700

    [gourl] add print header option

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gourl
 a simple url utility written by http://www.noteanddata.com for practicing go
+
+# sample usage 
+
+```
+gourl -h http://www.example.com
+
+```
+
--- gourl.go
+++ gourl.go
@@ -3,23 +3,43 @@ import "os"
 import "fmt"
 import "io/ioutil"
 import "net/http"
+import "flag"
 
 func main() {
   run()
 }
 
-func run() {
-  url := os.Args[1]
-  get(url)
+type Options struct {
+  printHeader bool
 }
 
-func get(url string) error {
+func run() {
+  url := os.Args[len(os.Args)-1]
+  var opt Options
+  flag.BoolVar(&opt.printHeader, "h", false, "print header")
+  flag.Parse()
+
+  //fmt.Printf("%+v \n", opt)
+
+  get(url, opt)
+}
+
+func get(url string, opt Options) error {
   resp, err := http.Get(url)
   if err != nil {
     fmt.Println(err)
     return err
   }
+  // print status code 
   fmt.Println(resp.Status)
+  
+  if opt.printHeader {
+    
+    for header, value := range resp.Header {
+        fmt.Println(header, value)
+    }
+  }
+  
   if resp.StatusCode == http.StatusOK {
     bodyBytes, err := ioutil.ReadAll(resp.Body)
     if err != nil {
